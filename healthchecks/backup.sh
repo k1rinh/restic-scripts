301ed920e267c60bd3d01f15083f12fbb73905e6
k1rin <git@k1rin.com>
2025-08-19 14:38:41 +0800

    feat: 添加 healthchecks 备份示例

---
+++ healthchecks/backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+source ../env.sh
+
+restic backup \
+    --tag "healthchecks" \
+    --group-by 'host,tags' \
+    --skip-if-unchanged \
+    --files-from ./includes.txt \
+    --exclude-file ./excludes.txt
