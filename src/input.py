6e6fb6273f989f974226bd8a0481e5a923471139
k1rin <git@k1rin.com>
2025-08-23 02:30:09 +0800

    wip

---
+++ src/input.py
@@ -0,0 +1,21 @@
+def new_check():
+    from .config import TIMEZONE
+    from .utils import get_hostname
+
+    return {
+        "name": input("name: "),
+        "schedule": new_oncalendar(),
+        "tags": input("tags(default: $hostname): ") or get_hostname(),
+        "desc": input("desc: "),
+        "grace": int(input("grace(default: 300): ") or 300),
+        "tz": input(f"tz(default: {TIMEZONE}): ") or TIMEZONE,
+    }
+
+
+def new_oncalendar():
+    from .utils import verify_oncalendar
+
+    while True:
+        oncalendar = input("oncalendar: ")
+        if verify_oncalendar(oncalendar):
+            return oncalendar
