2d0397fd2f0709e656394a628cf957e1139811e9
k1rin <git@k1rin.com>
2025-08-23 02:35:39 +0800

    wip

--- src/input.py
+++ src/input.py
@@ -3,12 +3,12 @@ def new_check():
     from .utils import get_hostname
 
     return {
-        "name": input("name: "),
+        "name": input("name> "),
         "schedule": new_oncalendar(),
-        "tags": input("tags(default: $hostname): ") or get_hostname(),
-        "desc": input("desc: "),
-        "grace": int(input("grace(default: 300): ") or 300),
-        "tz": input(f"tz(default: {TIMEZONE}): ") or TIMEZONE,
+        "tags": input("tags(default: $hostname)> ") or get_hostname(),
+        "desc": input("desc> "),
+        "grace": int(input("grace(default: 300)> ") or 300),
+        "tz": input(f"tz(default: {TIMEZONE})> ") or TIMEZONE,
     }
 
 
@@ -16,6 +16,6 @@ def new_oncalendar():
     from .utils import verify_oncalendar
 
     while True:
-        oncalendar = input("oncalendar: ")
+        oncalendar = input("oncalendar> ")
         if verify_oncalendar(oncalendar):
             return oncalendar
