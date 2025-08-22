99818da5429cfe0ad4e14a73a70e7637cebb6833
k1rin <git@k1rin.com>
2025-08-22 22:26:13 +0800

    wip

--- src/healthchecks.py
+++ src/healthchecks.py
@@ -3,3 +3,5 @@
 # - [ ] 创建 Checks 并获取 Ping URL。
 # - [ ] 列出当前 Host 下的所有 Checks 的时间点。
 
+# Reference:
+# - https://healthchecks.io/docs/api/
--- src/llm.py
+++ src/llm.py
@@ -1,2 +1,6 @@
 # TODO:
 # - [ ] 通过自然语言生成 OnCalendar Expression。
+
+# Reference:
+# - https://openrouter.ai/docs/quickstart
+# - https://github.com/cuu508/oncalendar/
